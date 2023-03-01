6b64f71157b42c1cf5d36025920a9ac60f6e23c3
EC2 Default User <ec2-user@ip-172-31-30-185.ap-northeast-1.compute.internal>
2023-03-01 03:44:58 +0000

    update

--- db/schema.rb
+++ db/schema.rb
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 2023_02_28_063037) do
   end
 
   create_table "books", force: :cascade do |t|
+    t.text "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
